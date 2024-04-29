17d5895698d464f3055b4427ebb3b998a608de41
Enoch Satrom <123826602+EnSatrom@users.noreply.github.com>
2024-04-29 16:15:13 -0500

    Ingredient lists (#4)
    * Added ingredient lists
    
    Lists ingredients for premade menu items
    
    Ingredients list for custom orders is there
    
    * Removed love
    
    found how to deal with trailing comma
    
    * changed margin in menu cards

--- friedRiceAtEddiesWebsite/main/views.py
+++ friedRiceAtEddiesWebsite/main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from main.models import Menu
+from main.models import Ingredient
+from main.models import Recipe
 import random, math
 
 def index(request):
@@ -10,5 +12,7 @@ def index(request):
 
 def menu(request):
     menu_list = Menu.objects.order_by('name')
-    context = {'menu': menu_list}
+    ingr_list = Ingredient.objects.order_by('name')
+    recipe_list = Recipe.objects.order_by('menu_id').prefetch_related('ingredients')
+    context = {'menu': menu_list, 'ingredients': ingr_list, 'recipes': recipe_list}
     return render(request, 'main/menu.html', context=context)
