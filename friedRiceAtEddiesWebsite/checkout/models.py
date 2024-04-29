78172d8b35b67985bec241a04bcb01d2e2524719
Gunnar Moody <moodygman04@gmail.com>
2024-04-29 15:57:41 -0500

    added back database changes

--- friedRiceAtEddiesWebsite/checkout/models.py
+++ friedRiceAtEddiesWebsite/checkout/models.py
@@ -1,16 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Member(models.Model):
     id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     tokens = models.IntegerField()
-    fname = models.CharField(max_length=50)
-    lname = models.CharField(max_length=50)
     phone_num = models.CharField(max_length=31)
-    email = models.EmailField()
-    address = models.CharField(max_length=200)
+    mailing_address = models.CharField(max_length=200)
 
     def __str__(self) -> str:
-        return str(self.username) + str(self.fname) + " " + str(self.lname)
+        return str(self.id) + ", " + str(self.user)
