74a8621ddc30d3ab698fb5365593d11e28a04615
EnochSatrom <enoch.satrom@gmail.com>
2024-04-29 15:54:01 -0500

    Added ingredient lists
    Lists ingredients for premade menu items
    
    Ingredients list for custom orders is there

--- friedRiceAtEddiesWebsite/main/views.py
+++ friedRiceAtEddiesWebsite/main/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from main.models import Menu
+from main.models import Ingredient
+from main.models import Recipe
 
 def index(request):
     return render(request, 'main/index.html')
 
 def menu(request):
     menu_list = Menu.objects.order_by('name')
-    context = {'menu': menu_list}
+    ingr_list = Ingredient.objects.order_by('name')
+    recipe_list = Recipe.objects.order_by('menu_id').prefetch_related('ingredients')
+    context = {'menu': menu_list, 'ingredients': ingr_list, 'recipes': recipe_list}
     return render(request, 'main/menu.html', context=context)
