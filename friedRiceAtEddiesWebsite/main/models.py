8de4bde385334a0fe4346958a8d0878151040749
Gunnar Moody <115500822+MoodyMan04@users.noreply.github.com>
2024-04-29 14:42:27 -0500

    Models+index (#2)
    * index.html + gitignore
    
    added:
    - apps in included apps in settings.py
    - paths to get to index.html
    - placeholder index.html code
    - gitignore for django
    - default view for index.html from main
    
    * fix isues
    
    * added model + superuser
    
    * adjusted gitignore
    
    adds db stuff
    
    * file structure + page changes
    
    added:
    - view for menu.html to have data of menu items
    - url to menu.html
    - url links to navbar
    - menu.html
        - has cards for menu items
    - images for carousel on index.html
    - static folders and files (img, css)
    - some custom styles in styles.css
    - more menu items in db
    
    changed:
    - file structure (templates, static, etc.)
    - index.html to have title, carousel of updates, and order here button
    -
    
    * changes to menu.html
    
    added:
    - section for building a custom rice bowl to menu.html (needs implementation)
    - styles for custom-order
    
    changed:
    - the view for menu slightly
    - the title of the menu.html slightly
    
    * added Customize button to menu.html
    
    (not yet fully implemented)
    
    * adjusted admin page and models
    
    added:
    - __str__() methods to all models in main/models.py and checkout/models.py
    
    changed:
    - admin.py to register models through a class, using list_display to better display values on the site
    
    * added stuff to db
    
    added more values to db + adjusted models.py slightly
    
    * adjusted templates
    
    changed:
    - div with paddings to <br /> tags
    (i know, i know)
    
    * Update db.sqlite3
    
    added more to db
    
    ---------
    
    Co-authored-by: Mason Myles <mason.myles98@gmail.com>

--- friedRiceAtEddiesWebsite/main/models.py
+++ friedRiceAtEddiesWebsite/main/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from checkout import models as checkout_models
 
 # Create your models here.
+class Ingredient(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+class Menu(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+class Recipe(models.Model):
+    id = models.AutoField(primary_key=True)
+    menu_id = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    ingredients = models.ManyToManyField(Ingredient)
+
+    def __str__(self) -> str:
+        return str(self.id)
+    
+
+class Way_Recieved(models.Model):
+    id = models.AutoField(primary_key=True)
+    type = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return str(self.type)
+
+class Order(models.Model):
+    id = models.AutoField(primary_key=True)
+    member_id = models.ForeignKey(checkout_models.Member, on_delete=models.CASCADE, null=True, blank=True)
+    way_recieved_id = models.ForeignKey(Way_Recieved, on_delete=models.CASCADE)
+    menu_items = models.ManyToManyField(Menu)
+    total = models.DecimalField(max_digits=5, decimal_places=2)
+    is_completed = models.BooleanField()
+    is_cash = models.BooleanField()
+
+    def __str__(self) -> str:
+        return str(self.id) + ", " + str(self.member_id) + ", " + str(self.way_recieved_id)
+  
--- friedRiceAtEddiesWebsite/main/views.py
+++ friedRiceAtEddiesWebsite/main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from main.models import Menu
 
-# Create your views here.
+def index(request):
+    return render(request, 'main/index.html')
+
+def menu(request):
+    menu_list = Menu.objects.order_by('name')
+    context = {'menu': menu_list}
+    return render(request, 'main/menu.html', context=context)
