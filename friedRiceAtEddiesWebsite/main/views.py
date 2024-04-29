a1cc06de369c69a3a9acc0c9aa709aa34baf5ed8
Mason Myles <59033177+minisodan@users.noreply.github.com>
2024-04-29 16:07:33 -0500

    Feature page (#3)
    * index.html + gitignore
    
    added:
    - apps in included apps in settings.py
    - paths to get to index.html
    - placeholder index.html code
    - gitignore for django
    - default view for index.html from main
    
    * fix isues
    
    * added model + superuser
    
    * adjusted gitignore
    
    adds db stuff
    
    * file structure + page changes
    
    added:
    - view for menu.html to have data of menu items
    - url to menu.html
    - url links to navbar
    - menu.html
        - has cards for menu items
    - images for carousel on index.html
    - static folders and files (img, css)
    - some custom styles in styles.css
    - more menu items in db
    
    changed:
    - file structure (templates, static, etc.)
    - index.html to have title, carousel of updates, and order here button
    -
    
    * changes to menu.html
    
    added:
    - section for building a custom rice bowl to menu.html (needs implementation)
    - styles for custom-order
    
    changed:
    - the view for menu slightly
    - the title of the menu.html slightly
    
    * added Customize button to menu.html
    
    (not yet fully implemented)
    
    * adjusted admin page and models
    
    added:
    - __str__() methods to all models in main/models.py and checkout/models.py
    
    changed:
    - admin.py to register models through a class, using list_display to better display values on the site
    
    * added stuff to db
    
    added more values to db + adjusted models.py slightly
    
    * adjusted templates
    
    changed:
    - div with paddings to <br /> tags
    (i know, i know)
    
    * Update db.sqlite3
    
    added more to db
    
    * poop
    
    ---------
    
    Co-authored-by: Gunnar Moody <moodygman04@gmail.com>
    Co-authored-by: Mason Myles <mason.myles98@gmail.com>

--- friedRiceAtEddiesWebsite/main/views.py
+++ friedRiceAtEddiesWebsite/main/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from main.models import Menu
+import random, math
 
 def index(request):
-    return render(request, 'main/index.html')
+    menu_list =  Menu.objects.order_by('name')
+
+    selected_items = random.sample(list(menu_list), math.floor(len(menu_list)/2))
+    return render(request, 'main/index.html', {'menu': selected_items})
 
 def menu(request):
     menu_list = Menu.objects.order_by('name')
