a46ebeeb5f79f391de08f8d2a6e49d15411ca2c9
Gunnar Moody <moodygman04@gmail.com>
2024-04-29 00:53:04 -0500

    file structure + page changes
    added:
    - view for menu.html to have data of menu items
    - url to menu.html
    - url links to navbar
    - menu.html
        - has cards for menu items
    - images for carousel on index.html
    - static folders and files (img, css)
    - some custom styles in styles.css
    - more menu items in db
    
    changed:
    - file structure (templates, static, etc.)
    - index.html to have title, carousel of updates, and order here button
    -

--- friedRiceAtEddiesWebsite/main/views.py
+++ friedRiceAtEddiesWebsite/main/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from main.models import Menu
 
 def index(request):
     return render(request, 'main/index.html')
+
+def menu(request):
+    menu_list = Menu.objects.order_by('name')
+    menu_dict = {'menu': menu_list}
+    return render(request, 'main/menu.html', menu_dict)
