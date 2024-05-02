4888880a14a9e91386eb989ff47af8fd727e5ee4
EnochSatrom <enoch.satrom@gmail.com>
2024-05-02 18:34:46 -0500

    added checkout buttons and tokens

--- friedRiceAtEddiesWebsite/checkout/urls.py
+++ friedRiceAtEddiesWebsite/checkout/urls.py
@@ -3,5 +3,6 @@ from checkout import views
 
 urlpatterns = [
     path('', views.view_cart, name='checkout'),
-    path('remove_from_cart/<item_id>', views.remove_from_cart, name='remove_from_cart')
+    path('remove_from_cart/<item_id>', views.remove_from_cart, name='remove_from_cart'),
+    path('process_order/<w_id>', views.process_order, name='process_order')
 ]
